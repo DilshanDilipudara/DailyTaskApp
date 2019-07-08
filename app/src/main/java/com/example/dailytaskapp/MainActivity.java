08bbbfb490897e174431e47caad7721d47c86336
DilshanDilipudara <gunawardhanaudara@gmail.com>
2019-07-08 22:34:08 +0530

    add id to firebase

--- app/src/main/java/com/example/dailytaskapp/MainActivity.java
+++ app/src/main/java/com/example/dailytaskapp/MainActivity.java
@@ -6,14 +6,25 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainActivity extends AppCompatActivity {
 
    private  Button btn_senddata;
-   private EditText text;
+   private EditText text,idtext;
+   private TextView textview;
+
 
    private Firebase reference;
 
@@ -27,15 +38,20 @@ public class MainActivity extends AppCompatActivity {
 
         btn_senddata = (Button) findViewById(R.id.btnsend);
         text = (EditText) findViewById(R.id.edittext);
+        textview = (TextView) findViewById(R.id.textvie);
+        idtext  = (EditText) findViewById(R.id.valuetext);
 
         btn_senddata.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                String task = text.getText().toString().trim();
-               String id =  reference.push().getKey();
+               String id =  idtext.getText().toString().trim();
 
                 if (!TextUtils.isEmpty(task)) {
-                    reference.child(id).setValue(task);
+
+                    Firebase childref = reference.child(id);
+                    childref.setValue(task);
+
                     Toast.makeText(MainActivity.this, "Sucussfully", Toast.LENGTH_SHORT).show();
 
                 }
@@ -43,8 +59,27 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "pleas enter the task", Toast.LENGTH_SHORT).show();
                 }
 
+
+                reference.addValueEventListener(new com.firebase.client.ValueEventListener(){
+                    @Override
+                    public void onDataChange(com.firebase.client.DataSnapshot datasnapshot){
+
+                        String text = datasnapshot.getValue(String.class);
+                        textview.setText(text);
+
+
+                    }
+
+                    @Override
+                    public  void onCancelled (FirebaseError firebasaerror){
+
+
+                    }
+                });
             }
         });
+
+
 
 
     }
